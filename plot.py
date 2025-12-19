d568ccd6e9879148b83aa147da6d21f67eb50b0d
adjeiameyaw <adjeiameyaw1974@gmail.com>
2025-12-18 22:06:25 -0500

    version_1

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-
+import streamlit as st
 import numpy as np
 import matplotlib.pylab as plt
 
@@ -6,6 +6,9 @@ import matplotlib.pylab as plt
 class plot():
     def initLine(ax):
       return ax.plot([], [], color = 'white')[0]
+
+    def initLine_imp(ax):
+      return []
 
     def animag(frame, trace, z):
       trace.set_data(np.real(z)[:frame], np.imag(z)[:frame])
@@ -15,6 +18,24 @@ class plot():
       trace.set_data(z[0][:frame], z[1][:frame])
       return trace
 
+    def animatimp(frame, trace, eqn):
+        X, Y, F = eqn
+
+        ax = plt.gca()
+
+        # Remove previous frame’s contour
+        if trace is not None:
+            for c in trace:
+                c.remove()
+
+        # Recompute updated implicit function for this frame
+        updated_F = F + 0.01 * frame  # <-- animate however you want
+
+        # Draw new contour
+        contour = ax.contour(X, Y, updated_F, levels=[0], colors="white")
+
+        return contour
+
     def initFigureWindow():
       fig, ax = plt.subplots(figsize = (5,5))
       #fig.canvas.manager.window.geometry('+1400+100')
@@ -22,19 +43,186 @@ class plot():
       ax.patch.set_facecolor('k')
       return fig,ax
 
+
+
 #equations
+
 class equations():
 
-    def first_eqn(a1,a2,a3,b1,b2,b3,c1,c2,c3,theta):
-      z = (a1 * np.exp((theta * b1 + c1) * 1j)
-           + a2 * np.exp((theta * b2 + c2) * 1j)
-           + a3 * np.exp((theta * b3 + c3) * 1j))
+    def hypospirograph(params,theta):
+      a, b , c  = params
+      x = (a-b)*np.cos(theta) + c*np.cos(((a-b)/b)*theta)
+      y = (a-b)*np.sin(theta) - c*np.sin(((a-b)/b)*theta)
 
-      return z
+      return x, y
+
+    def epispirograph(params,theta):
+      a, b , c  = params
+      x = (a + b) * np.cos(theta) + c * np.cos(((a + b) / b) * theta)
+      y = (a + b) * np.sin(theta) + c * np.sin(((a + b) / b) * theta)
+
+      return x, y
 
 
-    def sec_eqn(a,b,c,d,theta):
+    def sec_eqn(params,theta):
+        a, b, c, d = params
         x = a * np.cos(theta) - b * (np.cos(c * theta / d))
         y = a * np.sin(theta) - b * (np.sin(c * theta / d))
         return x, y
+
+
+    def Lissajous(params,theta):
+        a,b,c = params
+        x  = a * np.sin(b*theta + np.pi/2)
+        y = c * np.sin(theta)
+        return x, y
+
+
+    def third_eqn(params, theta):
+        a, b, c= params
+        x = a*np.sin(theta) + b*np.cos(c*theta)
+        y = a*np.cos(theta)
+        return x,y
+
+
+    def butterfly(params,theta):
+        """
+        a = 2, b = 4, c =5
+        :param theta:
+        :return:
+        """
+        a, b, c= params
+        x = np.sin(theta)*(np.exp(np.cos(theta)) - a*np.cos(b*theta) - np.sin(theta/c)**5)
+        y = np.cos(theta)*(np.exp(np.cos(theta)) - a*np.cos(b*theta) - np.sin(theta/c)**5)
+        return x,y
+
+
+    def fourth_eqn(params,theta):
+        a, b = params
+        x = np.sin(theta)*(np.tan((a*theta)/b))
+        y = np.cos(theta)*(np.tan((a*theta)/b))
+        return x,y
+
+
+    def fifth_eqn(params, theta):
+        a, b, c, d = params
+        x = a*np.cos(theta) - b*np.cos(c*theta/d)
+        y = a*np.sin(theta) - b*np.sin(c*theta/d)
+        return x,y
+
+
+    def sixth_eqn(params,theta):
+        a, b, c = params
+        x = a * np.sin(b * theta) * np.cos(np.cos(c * theta))
+        y = a * (np.cos(b * theta) ** 2) * np.sin(np.sin(c * theta))
+        return x, y
+
+
+    def seventh_eqn(params,theta):
+        a, b = params
+        x = a*theta + b*np.cos(b*theta) + 1/theta
+        y = a * theta + b * np.sin(b * theta) + 1 / theta
+        return x,y
+
+
+    def Eigth_eqn(params,theta):
+        a, b, c = params
+        x = np.cos(theta) + (1/a)*(np.cos(b*theta) + np.sin(c*theta))
+        y = np.sin(theta) + (1/a)*(np.sin(b*theta) + np.cos(c*theta))
+        return x,y
+
+    def nineth_eqn(params, theta):
+        """
+        a =20, b = 17
+        :param theta:
+        :return:
+        """
+        a , b = params
+        x = np.tan(b*theta)
+        y = a*x + 1/x
+        #z = a * (np.tan(b*theta)) + 1/np.tan(b*theta)
+        return x, y
+
+    def tenth_eqn(params, theta):
+        """
+        a = 9
+        :param theta:
+        :return:
+        """
+        a = params
+        x = theta
+        y = np.exp(x/a)
+        #z = np.exp(theta/a)
+        return x , y
+
+    def eleventh_eqn(theta):
+        """
+
+        :param theta:
+        :return:
+        """
+        x = theta
+        y = np.sqrt(x)
+        #z = np.sqrt(theta)
+        return x,y
+
+    def twelfth_eqn(params,theta):
+        """
+
+        :param theta: -4<=theta<=4
+        :a=4, b=1.88, c=2, d=4.3,
+        :return:
+        """
+        a,b,c,d= params
+        x = a*np.abs((np.cos(b*theta/np.pi)))**2.5
+        y = c*np.sin(np.sin(d*theta/np.pi))*(np.cos(b*theta/np.pi))**2
+
+        return x,y
+
+    def thirteenth_eqn(params, theta):
+        """
+        a = 7, b=3.48
+        :param theta:
+        :return:
+        """
+        a, b  = params
+        x = a*((np.sin(b*theta/np.pi))/(1+np.round((np.sin(theta/np.pi))**2)))
+        y = a*np.cos(np.round(theta/np.pi))*(np.sin(b*theta/np.pi))**4
+
+        return x,y
+
+    def fourteenth_eqn(params, theta):
+        """
+        a = 6, b=9.52, c = 4.8
+        :param theta: -8<=theta<=8
+        :return:
+        """
+        a, b ,c = params
+        x = a*np.sin(b*theta/np.pi)*np.round(np.sqrt(np.cos(np.cos(c*theta/np.pi))))
+        y = a*(np.cos(b*theta/np.pi)**4)*np.sin(np.sin(c*theta/np.pi))
+
+        return x,y
+
+    def fifteenth_eqn(params, theta):
+        """
+
+        :param theta:
+        :return:
+        """
+        a, b, c, d = params
+        x = a*np.sin(np.sin(a*theta/np.pi))*np.cos(np.cos(a*b*theta/np.pi)**2)
+        y = c*np.cos(d*theta/np.pi)**2*3*np.sin(theta)
+
+    ######## Implicit eqn
+
+    def imp_1(params):
+        a = params
+        x = np.linspace(-a[0], a[0], 400 )
+        y = np.linspace(-a[0], a[0], 400)
+        X, Y = np.meshgrid(x, y)
+
+        # Define the implicit function
+        F = Y ** 2 + np.sin(X + Y ** 2) - 1
+
+        return X,Y,F
 
--- stream.py
+++ stream.py
@@ -1,84 +1,378 @@
+
+
 import numpy as np
 import matplotlib.pylab as plt
 import matplotlib.animation as anim
-from matplotlib.animation import  FFMpegWriter, PillowWriter
-import  streamlit as st
+from matplotlib.animation import FFMpegWriter, PillowWriter
+import streamlit as st
 import streamlit.components.v1 as components
 from plot import *
+import plotly.graph_objects as go
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 import os
 import time
 
-a1 = 4
-a2 = 4
-a3 = 1.3
-
-b1 = 44
-b2 = -17
-b3 = -54
-
-c1 = 0
-c2= 0
-c3 = 0
 
 st.title("Welcome to Mathart")
 
-#Inputs
+#Place_holders
+eqn_func = None
+opt_transform =None
+values = []
+x = None
+y = None
+
+# Inputs
 option = st.selectbox(
     "What equation would you like to use for your art",
-    ("first equation", "second equation"),
+    ("hypospirograph", "epispirograph", "second equation", "Lissajous","third equation", "butterfly equation",
+     "fourth equation", "fifth equation", "sixth equation", "seventh equation", "eighth equation","nineth equation",
+     "tenth equation", "eleventh equation", "twelfth equation", "thirteenth equation","fourteenth equation"
+     ),
     index=None,
     placeholder="Select an equation",
 )
 
-if option == "first equation":
-    labels = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+if option == "hypospirograph":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned} x &= (a-b)\cos(\theta) + c\cos\!\left(\frac{a-b}{b}\,\theta\right) \\ 
+                y &= (a-b)\sin(\theta) - c\sin\!\left(\frac{a-b}{b}\,\theta\right) \end{aligned}
+            """)
+    labels = ['a','b','c']
     values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
-    a1, a2, a3, b1, b2, b3, c1, c2, c3 = values
+    eqn_func = equations.hypospirograph
+    opt_transform = "No"
 
-    fig, ax = plot.initFigureWindow()
-    trace = plot.initLine(ax)
+if option == "epispirograph":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned} x &= (a+b)\cos(\theta) + c\cos\!\left(\frac{a+b}{b}\,\theta\right) \\
+                 y &= (a+b)\sin(\theta) + c\sin\!\left(\frac{a+b}{b}\,\theta\right) \end{aligned}
+            """)
+    labels = ['a','b','c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.epispirograph
+    opt_transform = "No"
 
-    z1 = st.number_input("Enter z1 (-z1*theta): ", value=5)
-    z2 = st.number_input("Enter z2 (z2*theta): ", value=5)
-    theta = np.linspace(-z1*np.pi, z2*np.pi, 2000)
+elif option == "second equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                   x = a \cos(\theta) - b \cos\left(\frac{c \theta}{d}\right)\\                   
+                   y = a \sin(\theta) - b \sin\left(\frac{c \theta}{d}\right)
+               """)
+    labels = ['a', 'b', 'c', 'd']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.sec_eqn
+    opt_transform = "No"
 
-    #axis
-    st.write("Enter bounds [xmin, xmax, ymin, ymax]")
+elif option == "Lissajous":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                    \begin{aligned}
+                    x &= a \sin\left(b\theta + \frac{\pi}{2}\right) \\
+                    y &= c \sin(\theta)
+                    \end{aligned}
+                    """)
+    labels = ['a', 'b', 'c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.Lissajous
+    opt_transform = "No"
 
-    xmin = st.number_input("xmin", value=-20.0)
-    xmax = st.number_input("xmax", value=20.0)
-    ymin = st.number_input("ymin", value=-20.0)
-    ymax = st.number_input("ymax", value=20.0)
-
-    bounds = [xmin, xmax, ymin, ymax]
-
-    ax.axis(bounds) #[-x, x, -y, y] ask as input
-
-    if st.button("Create Art"):
-
-        eqn_1 = equations.first_eqn(a1,a2,a3,b1,b2,b3,c1,c2,c3,theta)
-    #eqn_2 = equations.sec_eqn(8,-6,8,3,theta)
-        myanimation = anim.FuncAnimation(fig, lambda frame:plot.animag(frame, trace,eqn_1),
-                                     frames = 2000, interval = 10 )
-
-        save_path = "Anim.mp4"
-        myanimation.save(save_path, fps=10)
-
-        # Display animation inline
-        components.html(myanimation.to_jshtml(), height=600)
-
-        # Add download button
-        with open(save_path, "rb") as f:
-            mp4_bytes = f.read()
-
-        st.download_button(
-            label="Download Animation",
-            data=mp4_bytes,
-            file_name="Anim.mp4",
-            mime="video/mp4"
-        )
+elif option == "third equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= a \sin(\theta) + b \cos(c\theta) \\
+                y &= a \cos(\theta)
+                \end{aligned}
+                """)
+    labels = ['a', 'b', 'c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.third_eqn
+    opt_transform = "No"
 
 
+elif option == "butterfly equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                    \begin{aligned}
+                    x &= \sin(\theta)\Big(\exp(\cos(\theta)) - a\cos(b\theta) - \sin\!\left(\tfrac{\theta}{c}\right)^{5}\Big) \\
+                    y &= \cos(\theta)\Big(\exp(\cos(\theta)) - a\cos(b\theta) - \sin\!\left(\tfrac{\theta}{c}\right)^{5}\Big)
+                    \end{aligned}
+""")
+    labels = ['a', 'b','c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.butterfly
+    opt_transform = "No"
+
+elif option == "fourth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= \sin(\theta)\,\tan\!\left(\frac{a\theta}{b}\right) \\
+                y &= \cos(\theta)\,\tan\!\left(\frac{a\theta}{b}\right)
+                \end{aligned}
+                """)
+    labels = ['a', 'b',]
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.fourth_eqn
+    opt_transform = "No"
+
+elif option == "fifth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= a \cos(\theta) - b \cos\left(\frac{c\theta}{d}\right) \\
+                y &= a \sin(\theta) - b \sin\left(\frac{c\theta}{d}\right)
+                \end{aligned}
+                """)
+    labels = ['a', 'b', 'c', 'd']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.fifth_eqn
+    opt_transform = "No"
+
+elif option == "sixth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= a \sin(b\theta)\,\cos\!\big(\cos(c\theta)\big) \\
+                y &= a \cos^{2}(b\theta)\,\sin\!\big(\sin(c\theta)\big)
+                \end{aligned}
+                """)
+    labels = ['a', 'b', 'c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.sixth_eqn
+    opt_transform = "No"
+
+elif option == "seventh equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= a\theta + b\cos(b\theta) + \frac{1}{\theta} \\
+                y &= a\theta + b\sin(b\theta) + \frac{1}{\theta}
+                \end{aligned}
+                """)
+    labels = ['a', 'b',]
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.seventh_eqn
+    opt_transform = "No"
+
+elif option == "eighth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+                \begin{aligned}
+                x &= \cos(\theta) + \frac{1}{a}\Big(\cos(b\theta) + \sin(c\theta)\Big) \\
+                y &= \sin(\theta) + \frac{1}{a}\Big(\sin(b\theta) + \cos(c\theta)\Big)
+                \end{aligned}
+                """)
+    labels = ['a', 'b','c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.Eigth_eqn
+    opt_transform = "No"
+
+elif option == "nineth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        z = a \tan(b\theta) + \frac{1}{\tan(b\theta)}
+        """)
+    labels = ['a', 'b']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.nineth_eqn
+    opt_transform = "No"
+
+elif option == "tenth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        z = e^{\tfrac{\theta}{a}}
+        """)
+    labels = ['a']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.tenth_eqn
+    opt_transform = "No"
+
+elif option == "eleventh equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        z = \sqrt{\theta}
+        """)
+    labels = []
+    values = []
+    eqn_func = equations.eleventh_eqn
+    opt_transform = "No"
+
+elif option == "twelfth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        \begin{aligned}
+        x &= a \big(\cos(b\theta)\big)^{2.5} \\
+        y &= c \,\sin\!\Big(\sin\!\left(\tfrac{d\theta}{\pi}\right)\Big)\,\big(\cos\!\left(\tfrac{b\theta}{\pi}\right)\big)^{2}
+        \end{aligned}
+        """)
+    labels = ['a', 'b', 'c', 'd']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.twelfth_eqn
+    opt_transform = st.selectbox("Would you like to perform a rotational transformation",
+                                 ("Yes", "No"),
+                                 index=None, )
+
+elif option == "thirteenth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        \begin{aligned}
+        x &= a \left(\frac{\sin\!\left(\tfrac{b\theta}{\pi}\right)}{1 + \operatorname{round}\!\Big(\sin^{2}\!\left(\tfrac{\theta}{\pi}\right)\Big)}\right) \\
+        y &= a \cos\!\Big(\operatorname{round}\!\left(\tfrac{\theta}{\pi}\right)\Big)\,\Big(\sin\!\left(\tfrac{b\theta}{\pi}\right)\Big)^{4}
+        \end{aligned}
+        """)
+    labels = ['a', 'b']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.thirteenth_eqn
+    opt_transform = st.selectbox("Would you like to perform a rotational transformation",
+                                 ("Yes", "No"),
+                                 index=None, )
+
+elif option == "fourteenth equation":
+    with st.expander("Parametric equation"):
+        st.latex(r"""
+        \begin{aligned}
+        x &= a \,\sin(b\theta)\,\operatorname{round}\!\Big(\sqrt{\cos\!\big(\cos(\tfrac{c\theta}{\pi})\big)}\Big) \\
+        y &= a \,\cos(b\theta)\,\sin\!\Big(\sin(c\theta)\Big)
+        \end{aligned}
+        """)
+    labels = ['a', 'b', 'c']
+    values = [st.number_input(f"Enter {label}", min_value=-100.0, step=0.1) for label in labels]
+    eqn_func = equations.fourteenth_eqn
+    opt_transform = st.selectbox("Would you like to perform a rotational transformation",
+                                 ("Yes", "No"),
+                                 index=None, )
 
 
+#fig, ax = plot.initFigureWindow()
+#trace = plot.initLine(ax)
+
+z1 = st.number_input("Enter z1 (-z1*theta): ", value=5)
+z2 = st.number_input("Enter z2 (z2*theta): ", value=5)
+theta = np.linspace(-z1 * np.pi, z2 * np.pi, 1800)
+
+
+# axis
+st.write("Enter bounds [xmin, xmax, ymin, ymax]")
+
+xmin = st.number_input("xmin", value=-20.0)
+xmax = st.number_input("xmax", value=20.0)
+ymin = st.number_input("ymin", value=-20.0)
+ymax = st.number_input("ymax", value=20.0)
+
+bounds = [xmin, xmax, ymin, ymax]
+
+
+if eqn_func is not None and values:
+    x, y = eqn_func(values, theta)
+
+#############if here
+
+#Initial traces
+if opt_transform =="Yes":
+    angle = st.text_input("Enter the angeles for transformation (e.g 15, 30, 45) ")
+    origin = st.text_input("Enter the origin of rotation (e.g   0, 0)")
+    angle_list, origin_list = [], []
+    if angle:
+        try:
+            angle_list = [float(x.strip()) for x in angle.split(',')]
+        except ValueError:
+            st.error("Please enter valid numbers separated by commas.")
+
+    if origin:
+        try:
+            origin_list = [float(x.strip()) for x in origin.split(',')]
+        except ValueError:
+            st.error("Please enter valid numbers for origin.")
+
+
+    rotated_list = []
+
+    coords = np.vstack((x,y))
+    if len(origin_list) == 2:
+        coords = coords - np.array(origin_list).reshape(2,1)
+    else:
+        st.warning("Please enter two numbers for the origin (e.g. 0, 0).")
+
+    for ang in angle_list:
+        rad = np.deg2rad(ang)
+        transform= np.array([[np.cos(rad), -np.sin(rad)],
+                             [np.sin(rad), np.cos(rad)
+
+        ]])
+        if len(origin_list) == 2:
+            rotated_list.append((transform @ coords) + np.array(origin_list).reshape(2,1) )
+
+    fig = go.Figure()
+    num_curves = len(rotated_list) + 1
+    paint = [mcolors.to_hex(cm.tab10(i % 10)) for i in range(num_curves)]
+
+    fig.add_trace(go.Scatter(x=[], y=[], mode="lines", name="Original", line=dict(color="white")))
+    #Add empty traces for each rotated curve
+    for j, color in enumerate(paint[1:]):
+        fig.add_trace(go.Scatter(x=[], y = [], mode="lines", name=f"Rotated{angle[j]}°", line=dict(color=color)))
+
+    #Animating the frames
+    frames = []
+    for k in range(0, len(x), 1):
+        frame_data = []
+
+        # Original curve up to k
+        frame_data.append(go.Scatter(x=x[:k], y=y[:k], mode="lines", line=dict(color="black")))
+
+        for j, rotated in enumerate(rotated_list):
+            frame_data.append(go.Scatter(x=rotated[0, :k], y=rotated[1, :k], mode="lines",line=dict(color=paint[j])))
+        frames.append(go.Frame(data=frame_data, name=str(k)))
+
+else:
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[], y=[], mode="lines", name="Original", line=dict(color="white")))
+    frames = []
+    if x is not None and y is not None:
+        for i in range(0, len(x), 1):  # step for speed
+            frames.append(go.Frame(
+                data=[
+                    go.Scatter(x=x[:i], y=y[:i], mode="lines", line=dict(color="blue")),
+                ],
+                name=str(i)
+            ))
+
+fig.frames = frames
+
+#Layout
+
+fig.update_layout(
+    xaxis=dict(range=[xmin, xmax]),
+    yaxis=dict(range=[ymin, ymax]),
+    title="Multiple Rotated Curves Animation",
+    updatemenus=[{
+        "buttons": [
+            {"args": [None, {"frame": {"duration": 20, "redraw": True},
+                             "fromcurrent": True, "transition": {"duration": 0}}],
+             "label": "Play",
+             "method": "animate"},
+            {"args": [[None], {"frame": {"duration": 0, "redraw": False},
+                               "mode": "immediate",
+                               "transition": {"duration": 0}}],
+             "label": "Pause",
+             "method": "animate"}
+        ],
+        "direction": "left",
+        "pad": {"r": 10, "t": 87},
+        "showactive": False,
+        "type": "buttons",
+        "x": 0.1,
+        "xanchor": "right",
+        "y": 0,
+        "yanchor": "top"
+    }]
+)
+
+#Layout with play/pause
+# --- Streamlit Display ---
+st.plotly_chart(fig, use_container_width=True)
 
